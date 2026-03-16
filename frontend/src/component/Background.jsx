51b1e5b04bc8b6e8a2237ec689c7f3b0e603dc46
Rafi Khan <wonderfulnatuare@gmail.com>
2026-03-16 23:10:42 +0600

    Update Background.jsx

--- frontend/src/component/Background.jsx
+++ frontend/src/component/Background.jsx
@@ -3,7 +3,12 @@ import back1 from "../assets/E-commerce MERN Assets/shirtwomen444.jpg"
 import back2 from "../assets/E-commerce MERN Assets/pantwoman144.jpg"
 import back3 from "../assets/E-commerce MERN Assets/t-shirtwomen3.jpg"
 import back4 from "../assets/E-commerce MERN Assets/jacket for women1.jpg"
-
+import back5 from "../assets/E-commerce MERN Assets/t-shirtman1.jpg"
+import back6 from "../assets/E-commerce MERN Assets/t-shirtman2.jpg"
+import back7 from "../assets/E-commerce MERN Assets/t-shirtman4.jpg"
+import back8 from "../assets/E-commerce MERN Assets/t-shirtwomen2.jpg"
+import back9 from "../assets/E-commerce MERN Assets/t-shirtwomen4.jpg"
+import back10 from "../assets/E-commerce MERN Assets/kidswear44.jpg"
 const Background = ({ herocount }) => {
   const images = [back1, back2, back3, back4]
 
